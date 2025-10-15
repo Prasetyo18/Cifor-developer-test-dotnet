51f3959208abf0c852b6316182aee4c85499da6d
Dwi Prasetyo <145000819+Prasetyo18@users.noreply.github.com>
2025-10-15 09:53:43 +0700

    Models

---
+++ Employee.cs
@@ -0,0 +1,11 @@
+namespace EmployeeApi.Models
+{
+    public class Employee
+    {
+        public string EmployeeId { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public string Department { get; set; } = null!;
+        public string Email { get; set; } = null!;
+    }
+}
